ac5c839d2f65a21aef5c83e3bfd2e57693494f73
iki1uc <entertrainer@web.de>
2026-07-02 03:20:43 +0200

    Add event listener to convert coreVec to JSON

--- main.js
+++ main.js
@@ -1,1 +1,10 @@
+// main.js
+
+import { coreVec } from "./core.js";
+
+document.getElementById("run").addEventListener("click", () => {
+  const data = coreVec.toJSON();
+  document.getElementById("out").textContent =
+    JSON.stringify(data, null, 2);
+});
 
