72cb59130b2337330426e99824a6eea7ec05956b
iki1uc <entertrainer@web.de>
2026-07-10 00:34:11 +0200

    Update ikl.js

--- ikl.js
+++ ikl.js
@@ -1,1 +1,15 @@
+import { Vec9 } from "./vec9.js";
 
+export const IKL_VEC = {
+    dir:   new Vec9(1,0,0),
+    mir:   new Vec9(0,1,0),
+    doch:  new Vec9(0,0,1),
+
+    egal:  new Vec9(1,1,0),
+    mov:   new Vec9(1,0,1),
+    nodi:  new Vec9(0,1,1),
+
+    use:   new Vec9(2,0,0),
+    user:  new Vec9(0,2,0),
+    REAL:  new Vec9(0,0,2)
+};
