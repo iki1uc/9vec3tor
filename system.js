bb28858334c31da8d0e2481faeef8aac1a9fb6e7
iki1uc <entertrainer@web.de>
2026-07-10 00:34:24 +0200

    Add systemVector function with axes and ikl

--- system.js
+++ system.js
@@ -1,1 +1,11 @@
+import { axesAll } from "./axes.js";
+import { IKL_VEC } from "./ikl.js";
+
+export function systemVector(Phi, phi, phi2){
+    return {
+        axes: axesAll(Phi, phi, phi2),
+        ikl: IKL_VEC,
+        status: "vec-ready"
+    };
+}
 
