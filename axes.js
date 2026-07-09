be4f720bebe4171aac233fb5f2e5e6fbf5169f71
iki1uc <entertrainer@web.de>
2026-07-10 00:33:55 +0200

    Add axis functions for Vec9 manipulation

--- axes.js
+++ axes.js
@@ -1,1 +1,14 @@
+import { Vec9 } from "./vec9.js";
+
+export function axisPhi(v){ return new Vec9(v, 0, 0); }
+export function axisphi(v){ return new Vec9(0, v, 0); }
+export function axisphi2(v){ return new Vec9(0, 0, v); }
+
+export function axesAll(Phi, phi, phi2){
+    return {
+        Phi: axisPhi(Phi),
+        phi: axisphi(phi),
+        phi2: axisphi2(phi2)
+    };
+}
 
